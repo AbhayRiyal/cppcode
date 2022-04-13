1c830663dfa403247fd859a0ecb1dc61ac54ed92
AbhayRiyal <abhay0038.be21@chitkara.edu.in>
2022-04-13 10:01:41 +0530

    oops questions

---
+++ projects/constructor.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class cube{
+    public:
+    int side;
+
+    cube(){
+        side=10;
+    }
+
+};
+
+
+int main(){
+    
+    cube a;
+    cout<<a.side;
+
+
+    return 0;
+}
